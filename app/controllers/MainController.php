ab1b93161f1d9d3c72f366010f5ed4c7c594c85b
GeremyBeaugas <geremy.beaugas@sts-sio-caen.info>
2021-12-01 10:55:30 +0100

    uiGroups

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -1,22 +1,64 @@
 <?php
 namespace controllers;
+ use Ajax\php\ubiquity\JsUtils;
+ use models\Organization;
+ use models\User;
  use services\dao\OrgaRepository;
+ use services\ui\UIGroups;
  use Ubiquity\attributes\items\di\Autowired;
+ use Ubiquity\attributes\items\router\Get;
+ use Ubiquity\attributes\items\router\Post;
  use Ubiquity\attributes\items\router\Route;
+ use Ubiquity\orm\DAO;
+ use Ubiquity\utils\http\URequest;
+ use Ubiquity\utils\http\USession;
 
  /**
   * Controller MainController
+  * @property JsUtils jquery
   */
 class MainController extends \controllers\ControllerBase{
 
     #[Route('/_default',name:'home')]
 	public function index(){
-		$this->loadDefaultView();
+		$this->jquery->renderDefaultView();
 	}
 
     #[Autowired]
     private OrgaRepository $repo;
     public function setRepo(OrgaRepository $repo): void {
         $this->repo = $repo;
+    }
+
+    private function showMessage(){
+
+    }
+
+    public function initialize() {
+        $this->ui=new UIGroups($this);
+        parent::initialize();
+    }
+
+    #[Get('new/user', name: 'new.user')]
+    public function newUser(){
+        $this->ui->newUser('frm-user');
+        $this->jquery->renderView('main/vForm.html',['formName'=>'frm-user']);
+    }
+
+    #[Post('new/user', name: 'new.userPost')]
+    public function newUserPost(){
+        $idOrga=USession::get('idOrga');
+        $orga=DAO::getById(Organization::class,$idOrga,false);
+        $user=new User();
+        URequest::setValuesToObject($user);
+        $user->setEmail(\strtolower($user->getFirstname().'.'.$user->getLastname().'@'.$orga->getDomain()));
+        $user->setOrganization($orga);
+        if(DAO::insert($user)){
+            $count=DAO::count(User::class,'idOrganization= ?',[$idOrga]);
+            $this->jquery->execAtLast('$("#users-count").html("'.$count.'")');
+            $this->showMessage("Ajout d'utilisateur","L'utilisateur $user a été ajouté à l'organisation.",'success','check square outline');
+        }else{
+            $this->showMessage("Ajout d'utilisateur","Aucun utilisateur n'a été ajouté",'error','warning circle');
+        }
     }
 }
